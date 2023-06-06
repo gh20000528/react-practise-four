d6cdfd60dccca89cef8e2579c2b53911958c782d
han sheng <gh890528@gmail.com>
2023-06-06 17:52:51 +0800

    Learn context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React , {useState , useEffect} from 'react'
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home'
+import AuthContext from './store/auth-context'
 import MainHeader from './components/MainHeader/MainHeader'
 
 function App() {
@@ -20,16 +21,23 @@ function App() {
   }
 
   const logoutHandler = ()=> {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false)
   }
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler}/>
-        <main>
-          { !isLoggedIn && <Login onLogin={loginHandler}></Login> }
-          { isLoggedIn && <Home onLogout={logoutHandler}></Home> }
-        </main>
-    </React.Fragment>
+
+    <AuthContext.Provider 
+    value={{
+      isLoggedIn: isLoggedIn,
+      onLogout: logoutHandler
+    }}>
+      <MainHeader/>
+      <main>
+        { !isLoggedIn && <Login onLogin={loginHandler}></Login> }
+        { isLoggedIn && <Home onLogout={logoutHandler}></Home> }
+      </main>
+    </AuthContext.Provider>
+
   );
 }
 
